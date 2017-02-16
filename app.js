eb19e9ee081c6a8c84a19abd96c16b72de063a89
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2017-02-16 12:21:47 -0500

    create skeleton node app, and enable express server and sockets

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io')(server);
+
+//Express setup
+server.listen(process.env.PORT || 3000);
+app.use(express.static(__dirname + '/client'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+//Sockets Setio
+io.sockets.on('connection', function(){
+  console.log('we have connection');
+});
+
