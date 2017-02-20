6f0c48c1873b7ef2fe47e25bfc2ab3dbb52b6fc7
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2017-02-20 04:43:39 -0500

    Started to refactor to enable game play by using sockets, to ensure that no data is passed through front end

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express'),
-    app = express(),
-    http = require('http'),
-    server = http.createServer(app),
-    io = require('socket.io')(server),
-    bodyParser = require("body-parser");
+var express     = require('express'),
+    app         = express(),
+    http        = require('http'),
+    server      = http.createServer(app),
+    io          = require('socket.io')(server),
+    bodyParser  = require('body-parser'),
+    dictionary  = require('./dictionary/dictionaryWords');
 
 //---------------------------------Express setup---------------------------------\\
 server.listen(process.env.PORT || 3000);
@@ -26,13 +27,97 @@ app.get('*', function(req, res) {
 
 
 //-------------------Socket.io Connection Setup------------------------------------\\
-io.sockets.on('connection', function(){
+io.on('connection', function(socket){
   console.log('we have connection');
 
-  // socket.on('disconnect', function(){
-  //   console.log('user disconnected');
-  // });
+  socket.on('gameStart', function(data){
+    //get option for game from button clicked
+    var option = data.option;
+
+    //generate random number to correspond with index of word in dictionary
+    var randomNumber = Math.floor((Math.random() * dictionary.words.length));
+    
+    //set secret word that player has to guess
+    var secretWord = dictionary.words[randomNumber];
+  
+    // Print dashes on screen that corresponds with word length
+    // Don't send object to prevent user access in browser tools
+    io.emit('printDashes',  {wordLength: secretWord.length});
+
+    //start clock
+    io.emit('startClock');
+    
+    //show input box for word
+
+    // create a new game
+    // //Loop for 6 guesses
+    // var numGuessesRemaining = 6;
+
+    // while(numGuesses >= 1 ){
+    //   //get input from user
+    //   //check to see if input
+    //   if(option === "option1"){
+
+    //   } else {
+
+    //   }
+    // }
+    io.on('stopGame', function(){
+      //
+
+    });
+
+    io.on('setUserInput', function(data){
+      //call appropriate game function
+
+    });
+
+    //this test works
+    //io.emit('changeDashesToLetters', {indices: [2, 3], letter: "l"});
+  });
+  
+  
+
+
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+
+//-------------------------------Game Play Functions-----------------------------------------\\
+
+function gameFlow(){
+  var guessesRemaining = 6;
+  while (guessesRemaining >= 0){
+    //display get input from user
+
+  }
+};
+
+//Determines the indices of all letter occurances in a word
+function getIndicesOfLetterInWord(wordData, letterData){
+  var letterIndices = [];
+
+  for(var i = 0; i < data1.word.length; i++){
+    if(wordData.word[i] === letterData.toLowerCase()){
+      letterIndices.push(i);
+    }
+  }
+  return letterIndices;
+};
+
+
+//Game.prototype.stop = function(){
+  //save time to database Only if winner is the player
+  //If option 1 
+    //grab time from dom and save to database
+  //If option 2
+    //grab time from dom and
+//}
+
+
 
 
 
--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,11 +1,167 @@
 
-//var socket = io();
+var socket = io();
 
 //Displays the current year in the footer of the Index.html
 function postPresentYear(){
-  var year = document.getElementById("copyright-year");
+  var year = document.getElementById('copyright-year');
   year.innerText = new Date().getFullYear();
 };
+
+//Triggers function to intitiate Leader Board display
+function showLeaderBoard(){
+  socket.emit('getLeaders');
+}
+
+//Triggers game start
+function startGame(button){
+  //get option from value
+  var option = button.value;
+
+  // hide game options div
+  var gameOptions = document.getElementById('game-options');
+  gameOptions.style.display = 'none';
+
+  //show user-input box
+  var userInput = document.getElementById('user-input');
+  userInput.style.display = 'block';
+
+  //emit gameStart to backend with option values
+  socket.emit('gameStart', {option: option});
+
+
+  //show End Game Submit button
+  var endButton = document.getElementById('end-game-button');
+  endButton.style.display = 'block';
+
+};
+
+//Triggers Game end
+function endGame(){
+  //emit 'stopGame'
+  socket.emit('stopGame');
+  
+  //stop Timer
+  socket.emit('stopClock');
+
+  //hide endGame Button
+  var endButton = document.getElementById('end-game-button');
+  endButton.style.display = 'none';
+
+  //show game options
+  var gameOptions = document.getElementById('game-options');
+  gameOptions.style.display = 'block';
+
+  //hide user-input box
+  var userInput = document.getElementById('user-input');
+  userInput.style.display = 'none';
+}
+
+//validate user enters a letter only, and emit letter to backend
+function validateInput(){
+  //ensure letter is displayed,
+    //if not, display error message to enter a letter
+    // if letter 
+    //emit letter
+  var letterGuessed = document.getElementById('letter-guessed').value;
+
+  //letters only Regex
+  var letters = /^[A-Za-z]+$/;
+
+  
+  if(letterGuessed.match(letters)){
+    //send letter to backend for testing in game play 
+    socket.emit('setUserInput', {letter: letterGuessed});
+  } else {
+    //FIX THIS!!
+    alert('Enter letter characters only');
+    //show user-input box
+    var userInput = document.getElementById('user-input');
+    userInput.style.display = 'block';
+  }
+  
+}
+
+function updateTimeElapsed(secondsElapsed){
+  var timeDisplay = document.getElementById('time-elapsed-display');
+  var minutes; 
+  var timeElapsed;
+
+  //convert to minutes and seconds
+};
+
+//Displays the string as either the correct letters in word or incorrectly guessed letters on DOM
+function displayLettersOnScreen(str, isCorrectLetter){
+  var textContent;
+
+  if(isCorrectLetter){
+    textContent= document.getElementById('word-display');
+  } else {
+    textContent= document.getElementById('letters-guessed-display');
+  }
+
+  textContent.innerHTML = str;
+}
+
+
+socket.on('displayLeaderBoard', function(data){
+  
+  //make leader-board div visible
+  var board = document.getElementById('leader-board');
+  var endButton = document.getElementById('end-game-button');
+  board.style.display = 'block';
+  
+  //traverse array of leaders
+  //display on board
+});
+
+
+
+// Print dashes on screen that corresponds with word length
+socket.on('printDashes', function (data){
+  //console.log(word);
+  var letters = document.getElementById('word-display');
+  var dashes = '';
+
+  for(var i = 0; i < data.wordLength; i++){
+    if(i === data.wordLength -1){
+      dashes += '_';
+    }else{
+      dashes += '_' + ' ';
+    }
+  } 
+  letters.innerHTML = dashes;
+});
+
+
+
+//Replaces dashes to letters of respective index and returns string with indices and dashes
+socket.on('changeDashesToLetters', function(data){
+  var letter = data.letter;
+  var indices = data.indices;
+
+  var displayedChars = document.getElementById('word-display');
+  var displayedText = displayedChars.innerText.split("");
+  
+  for(var i = 0; i < indices.length; i++){
+    if(indices[i] === 0){
+      displayedText[i] = letter.toUpperCase();
+    }else {
+      displayedText[indices[i] * 2] =  letter.toUpperCase();
+    }
+  }
+
+  displayLettersOnScreen(displayedText.join(''), true);
+});
+
+
+socket.on('displayLetters', function(data){
+  displayLettersOnScreen(data.str, data.isCorrectLetter);
+});
+
+socket.on('startClock', function(){
+  //stet
+  var startTime; 
+});
 
 
 
